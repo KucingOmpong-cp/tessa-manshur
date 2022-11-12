bb58a64200bdd9096484405f02b1d090eb233a16
Muhammad Manshur <manshur@nusantarabetastudio.com>
2022-11-12 18:58:47 +0700

    add countdown function

---
+++ _assets/js/app.js
@@ -0,0 +1,24 @@
+// Mengatur waktu akhir perhitungan mundur
+let countDownDate = new Date("February 12, 2023 08:00:00").getTime();
+
+// Memperbarui hitungan mundur setiap 1 detik
+let x = setInterval(function() {
+
+  // Untuk mendapatkan tanggal dan waktu hari ini
+  let now = new Date().getTime();
+      
+  // Temukan jarak antara sekarang dan tanggal hitung mundur
+  let distance = countDownDate - now;
+      
+  // Perhitungan waktu untuk hari, jam, menit dan detik
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      
+  // Keluarkan hasil dalam elemen dengan id = "demo"
+  document.getElementById("day").innerHTML = days;
+  document.getElementById("hour").innerHTML = hours;
+  document.getElementById("min").innerHTML = minutes;
+  document.getElementById("sec").innerHTML = seconds;
+}, 1000);
